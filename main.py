c25c67254ac869732b7f30704853f3f425cab605
Liam Hampton <liam.hampton@hotmail.co.uk>
2024-04-24 14:12:12 +0100

    move api version to .env

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2023-12-01-preview",
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 )
 
